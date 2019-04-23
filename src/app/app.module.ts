341d5a2410fcc28da24804207b985a1d0e3cdebb
yvelines <lfmparente@gmail.com>
2019-04-23 13:36:13 +0200

    add start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './courses/home/home.component';
+import { AboutComponent } from './about/about.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CoursesCardListComponent } from './courses/courses-card-list/courses-card-list.component';
+import {CourseComponent} from "./courses/course/course.component";
+import {
+    MatDatepickerModule,
+    MatDialogModule,
+    MatInputModule, MatListModule, MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule
+} from "@angular/material";
+import {CoursesService} from "./courses/services/courses.service";
+import {HttpClientModule} from "@angular/common/http";
+import {CourseResolver} from "./courses/services/course.resolver";
+import { CourseDialogComponent } from './courses/course-dialog/course-dialog.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatMomentDateModule} from "@angular/material-moment-adapter";
+import {CoursesModule} from './courses/courses.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    declarations: [
+      AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatButtonModule,
+        CoursesModule,
+        AppRoutingModule
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {
+}
